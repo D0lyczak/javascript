68097e6d91c785c59dc5d405bfb25f34e9f1a3f0
DorianŁyczak <08dlyczak@gmail.com>
2025-09-12 13:38:31 +0200

    Update lyczak_zewnetrzny.js

--- lyczak_zewnetrzny.js
+++ lyczak_zewnetrzny.js
@@ -1,2 +1,2 @@
-document.write("ostatnia modyfikacja strony".fontcolor("blue").bold().strike().fontsize(5)+"<br>"); 
+document.write("ostatnia modyfikacja strony".fontcolor("blue").strike().fontsize(6)+"<br>"); 
 document.write(document.lastModified); 
